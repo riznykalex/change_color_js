264d11e72969bf15a0f75bd402e46ddb992a1676
riznykalex <58046631+riznykalex@users.noreply.github.com>
2020-11-21 16:07:23 +0200

    Add files via upload

--- chromakey/change.js
+++ chromakey/change.js
@@ -1,50 +1,81 @@
-let processor = {
-    timerCallback: function(){
-        if(this.video.paused || this.video.ended){
-           return; 
-        }
-        this.computeFrame();
-        let self = this;
-        setTimeout(function(){
-            self.timerCallback
-        }, 0);
-    },
-    doLoad: function(){
-        this.video = document.querySelector("#video");
-        this.c1 = document.querySelector('#c1');
-        this.ctx1 = this.c1.getContext('2d');
+let startButton;
+let canvas;
+let context;
+let video;
+let videoBg;
+let canPlay;
+let applyEffect;
 
-        this.c2 = document.querySelector('#c2');
-        this.ctx2 = this.c2.getContext('2d');        
-        let self = this;
-        this.video.addEventListener('play', function(){
-            self.width = self.video.videoWidth;
-            self.height = self.video.videoHeight;
-            self.timerCallback();
+function init() {
+    startButton = document.getElementById('StartButton');
+    toggleButton = document.getElementById('ToggleButton');
+    canvas = document.getElementById('Canvas');
+    context = canvas.getContext('2d');
+    video = document.getElementById('SourceVideo');
+    videoBg = document.getElementById('bgVid');
 
-        });
-    },
-    computeFrame: function(){
-        this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
-        let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
-        frame.crossOrigin="anonymous";
-        let l = frame.data.length / 4;
+    video.muted = true;
 
-        for(let i = 0; i < l; i++){
-            let r = frame.data[i * 4 + 0];
-            let g = frame.data[i * 4 + 1];
-            let b = frame.data[i * 4 + 2];
-            if (g > 120 && r < 55) {frame.data[i * 4 + 3] = 0;
-            };
-        };
-        this.ctx2.putImageData(frame, 0, 0);
-        return;
 
+
+    if (video.readyState >= 3) {
+        readyToPlay();
+    } else {
+        video.addEventListener('canplay', readyToPlay);
     }
-};
 
-document.addEventListener("DOMContentLoaded", () =>{
-    processor.doLoad();
-});
+    startButton.addEventListener('click', function () {
+        console.log(canPlay);
+        if (!canPlay) return;
 
-//"C:\Program Files\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
+
+        video.style.width = '640px';
+        video.loop = true;
+        video.play();
+        videoBg.loop = true;
+        videoBg.play();
+
+        drawFrame(video);
+    });
+
+    toggleButton.addEventListener('click', function () {
+        applyEffect = !applyEffect;
+    });
+}
+
+function readyToPlay() {
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    canPlay = true;
+}
+
+function drawFrame(video) {
+    context.drawImage(video, 0, 0);
+
+    if (applyEffect) {
+        let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        invertColors(imageData.data);
+        context.putImageData(imageData, 0, 0);
+    }
+
+    setTimeout(function () {
+        drawFrame(video);
+    }, 10);
+}
+
+
+function invertColors(data) {
+    for (let i = 0; i < data.length; i += 4) {
+        var avg = (data[i] + data[i + 1] + data[i + 2])/3
+        //if (data[i] <= 120  && data[i + 1] > 150 && data[i + 2] < 120) {
+        //if (data[i + 1] > 120 && data[i] + data[i + 2] < data[i + 1] +70 ) {
+        if (data[i+1]  - avg > 36) {
+            data[i + 3] = 0 ;
+        }
+    }
+}
+
+window.addEventListener('load', init);
+
